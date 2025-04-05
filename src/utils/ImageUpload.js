903c0ff93a80811a8ce55dd96370f54a8bbe73d9
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-04-05 18:26:56 +0600

    project done

--- src/utils/ImageUpload.js
+++ src/utils/ImageUpload.js
@@ -1,36 +1,36 @@
 export const imageUpload = async (imageFile) => {
   const reader = new FileReader();
-  
+
   return new Promise((resolve, reject) => {
     reader.onload = async (event) => {
-      const base64Image = event.target.result.split(',')[1];
-      
+      const base64Image = event.target.result.split(",")[1];
+
       try {
         const formData = new FormData();
-        formData.append('image', base64Image);
-        
+        formData.append("image", base64Image);
+
         const response = await fetch(
-          `https://api.imgbb.com/1/upload?key=b5909081a1a389623667a64a68fc33d2`,
+          `https://api.imgbb.com/1/upload?key=7ea608de6099c1a44ac39bb34b1d5f36`,
           {
-            method: 'POST',
+            method: "POST",
             headers: {
-              'X-Requested-With': 'XMLHttpRequest'
+              "X-Requested-With": "XMLHttpRequest",
             },
-            body: formData
+            body: formData,
           }
         );
-        
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        
+
         const data = await response.json();
         resolve(data);
       } catch (error) {
         reject(error);
       }
     };
-    
+
     reader.onerror = reject;
     reader.readAsDataURL(imageFile);
   });
